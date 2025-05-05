3fc51381c1b6963e86e5cbcf935979c1e4a3e1a6
Niumiueb <n.ebrahiminoor@gmail.com>
2025-05-05 08:36:38 -0400

    need to fix the intervals on about page

--- jsForAbout.js
+++ jsForAbout.js
@@ -1,2 +1,31 @@
-import {myFunction} from "./utils.js";
-window.onscroll = function() {myFunction()};
+import { myFunction } from './utils.js';
+window.onscroll = function() { myFunction(); };
+
+
+const circle = document.querySelector('.circle-progress');
+const radius = circle.r.baseVal.value;
+console.log(radius)
+const circumference = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = `${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`; 
+
+let current = 0;
+const target = 75; 
+
+
+function setProgress(percent) {
+    const offset = circumference - (percent / 100) * circumference;
+    circle.style.strokeDashoffset = offset;
+    
+    document.getElementById('percentage').textContent = `${percent}%`;
+}
+
+const interval = setInterval(() => {
+    if (current <= target) {
+      setProgress(current);
+      current += 1;
+    } else {
+      clearInterval(interval);
+    }
+}, 20);
