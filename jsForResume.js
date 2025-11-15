bae034b0354a4abd11673eaff564151308ce35a5
NiushaEbrahimi <n.ebrahiminoor@gmail.com>
2025-11-15 10:16:29 -0800

    removing the files except for Portfolio

--- jsForResume.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', (event) => {
-    const x = event.clientX; // X-coordinate relative to the viewport
-    const y = event.clientY; // Y-coordinate relative to the viewport
-    console.log(`Mouse Position: X: ${x}, Y: ${y}`);
-});
