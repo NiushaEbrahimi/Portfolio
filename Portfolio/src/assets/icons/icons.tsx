f4194b9471ae9a6cdcd7ea91de9546566f1576af
NiushaEbrahimi <n.ebrahiminoor@gmail.com>
2025-12-16 03:59:41 -0800

    vercel

--- Portfolio/src/assets/icons/icons.tsx
+++ Portfolio/src/assets/icons/icons.tsx
@@ -1,6 +1,6 @@
 import reactImg from "./React.png";
 import pythonImg from "./python.png";
-import bootstrap from "./bootstrapMUI.png";
+import bootstrap from "./bootstrapMUI copy.png";
 import django from "./django.png";
 import djangoRest from "./django rest.png"
 import htmlCss from "./html css.png";
